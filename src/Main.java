ee30fc8f5c637da9d3f9d0286424d77fcf001148
Ed Travalia <33702136+etravalia@users.noreply.github.com>
2020-03-06 16:48:32 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// Create a zoo object
-		Zoo thisZoo = new Zoo(10,10);
+		Zoo thisZoo = new Zoo(10);
 		int totalAnimals = 0;
 		
 		populateMyZoo(thisZoo);
